9a89e2e16fbb5c421b2255cd99f4f27f40641ac1
Simon <simonr1337@gmail.com>
2023-08-27 18:19:44 +0200

    proof of concept works

--- evil-magic.el
+++ evil-magic.el
@@ -91,7 +91,7 @@ new text arrives")
     (setq evil-magic-gaze-pos (cons
                                (* (car expr) monitor-width)
                                (* (cadr expr) monitor-height)))
-    (message "%s" evil-magic-gaze-pos)
+                                        ;(message "%s" evil-magic-gaze-pos)
     ))
 
 (defun evil-magic-server-sentinel (proc msg)
@@ -116,6 +116,83 @@ new text arrives")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;; IPC Code end ;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+
+;;;;;;;;;;;;;;;;;;;;;;; Helpers from https://github.com/andreamerello/emacs/blob/master/modules/pos-tip.el
+
+
+(defvar pos-tip-frame-offset-array [nil nil nil nil]
+  "Array of the results of `pos-tip-calibrate-frame-offset'. They are
+recorded only when `pos-tip-frame-top-left-coordinates' is called for a
+non-X but graphical frame.
+
+The 2nd and 4th elements are the values for frames having a menu bar.
+The 3rd and 4th elements are the values for frames having a tool bar.")
+
+
+(defvar pos-tip-saved-frame-coordinates '(0 . 0)
+  "The latest result of `pos-tip-frame-top-left-coordinates'.")
+
+
+(defun pos-tip-window-system (&optional frame)
+  "The name of the window system that FRAME is displaying through.
+The value is a symbol---for instance, 'x' for X windows.
+The value is nil if Emacs is using a text-only terminal.
+
+FRAME defaults to the currently selected frame."
+  (let ((type (framep (or frame (selected-frame)))))
+    (if type
+        (and (not (eq type t))
+             type)
+      (signal 'wrong-type-argument (list 'framep frame)))))
+
+(defun pos-tip-frame-top-left-coordinates (&optional frame)
+  "Return the pixel coordinates of FRAME as a cons cell (LEFT . TOP),
+which are relative to top left corner of screen.
+
+Return nil if failing to acquire the coordinates.
+
+If FRAME is omitted, use selected-frame.
+
+Users can also get the frame coordinates by referring the variable
+`pos-tip-saved-frame-coordinates' just after calling this function."
+  (let ((winsys (pos-tip-window-system frame)))
+    (cond
+     ((null winsys)
+      (error "text-only frame: %S" frame))
+     ((eq winsys 'x)
+      (condition-case nil
+          (with-current-buffer (get-buffer-create " *xwininfo*")
+            (let ((case-fold-search nil))
+              (buffer-disable-undo)
+              (erase-buffer)
+              (call-process shell-file-name nil t nil shell-command-switch
+                            (format "xwininfo -display %s -id %s"
+                                    (frame-parameter frame 'display)
+                                    (frame-parameter frame 'window-id)))
+              (goto-char (point-min))
+              (search-forward "\n  Absolute")
+              (setq pos-tip-saved-frame-coordinates
+                    (cons (string-to-number (buffer-substring-no-properties
+                                             (search-forward "X: ")
+                                             (line-end-position)))
+                          (string-to-number (buffer-substring-no-properties
+                                             (search-forward "Y: ")
+                                             (line-end-position)))))))
+        (error nil)))
+     (t
+      (let* ((index (+ (pos-tip-normalize-natnum
+                        (frame-parameter frame 'menu-bar-lines) 0)
+                       (pos-tip-normalize-natnum
+                        (frame-parameter frame 'tool-bar-lines) 1)))
+             (offset (or (aref pos-tip-frame-offset-array index)
+                         (aset pos-tip-frame-offset-array index
+                               (pos-tip-calibrate-frame-offset frame)))))
+        (if offset
+            (setq pos-tip-saved-frame-coordinates
+                  (cons (+ (eval (frame-parameter frame 'left))
+                           (car offset))
+                        (+ (eval (frame-parameter frame 'top))
+                           (cdr offset))))))))))
 
 (defun evil-magic-get-monitor-resolution()
   "Get the resolution of the active monitor.
@@ -163,7 +240,7 @@ new text arrives")
   (let ((dx (- (car pos2) (car pos1)))
         (dy (- (cdr pos2) (cdr pos1))))
     (sqrt (+ (* dx dx) (* dy dy)))))
-
+(list 'test)
 (defun evil-magic-pixel-gaze-distance (pixel-pos)
   "Returns the distance between the pixel position and the gaze position."
   (evil-magic-pixel-distance pixel-pos evil-magic-gaze-pos))
@@ -176,32 +253,29 @@ new text arrives")
   "Returns the pixel position of the match of str in the current buffer
    that is closest to the gaze position."
   (let ((match-positions (evil-magic-matches-pixel-positions str)))
-    (apply #'min match-positions :predicate #'evil-magic-pixel-comparator)))
+    (car (sort match-positions #'evil-magic-pixel-comparator))))
+
+(defun evil-magic-frame-top-offset ()
+  "Returns the pixel offset to the emacs frame in pixels."
+  (cdr (pos-tip-frame-top-left-coordinates)))
+
+(defun evil-magic-pixel-to-buffer-pos (pos)
+  (posn-point (posn-at-x-y (car pos) (- (cdr pos) (evil-magic-frame-top-offset)) nil t)))
 
 (defun evil-magic-read-mind (str)
   "Magically moves the cursor the the match of the search string that's
    closest to where the user is looking."
-  (let* ((match-pixel-pos (evil-magic-closest-match-to-gaze str))
-         (match-buffer-pos (posn-point (posn-at-x-y (car match-pixel-pos) (cdr match-pixel-pos)))))
-    (goto-char match-buffer-pos)))
-
-(posn-point (posn-at-x-y 395 64))
-(point)
-
-(defun highlight-position (position)
-  "Highlight the character at the given position."
-  (let ((overlay (make-overlay position (1+ position))))
-    (overlay-put overlay 'face '(:background "yellow"))))
+  (let ((match-pixel-pos (evil-magic-closest-match-to-gaze str)))
+    (goto-char (evil-magic-pixel-to-buffer-pos match-pixel-pos))))
 
 (evil-magic-read-mind "str")
 (evil-magic-closest-match-to-gaze "str")
 (evil-magic-search-matches "str")
 (window-absolute-pixel-position (point))
 (evil-magic-matches-pixel-positions "str")
-(highlight-position (window-start))
+(evil-magic-pixel-to-buffer-pos '(100 . 100))
 
-(let* ((match-pixel-pos (cadr (evil-magic-matches-pixel-positions "(defun evil-magic-matches-pixel-positions")))
-       (match-buffer-pos (posn-point (posn-at-x-y (car match-pixel-pos) (cdr match-pixel-pos)))))
-  (highlight-position match-buffer-pos))
+;;(let ((match-pixel-pos (car (last(evil-magic-matches-pixel-positions "pos")))))
+;;  (goto-char (evil-magic-pixel-to-buffer-pos match-pixel-pos)))
 
 (provide 'evil-magic-server)
